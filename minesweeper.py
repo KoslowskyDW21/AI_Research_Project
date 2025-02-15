38f3e19635ce9fcdf24f0caa290fd1db58121488
KoslowskyDW21 <KoslowskyDW21@gcc.edu>
2025-02-15 14:00:53 -0500

    Added  backtracking algorithm

--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,12 @@
-import random
+import copy
+import time
 
+numAllocations = 0
 
 class boardSpot(object):
     value = 0
     selected = False
-    mine = False
+    flagged = False
 
     def __init__(self):
         self.selected = False
@@ -16,41 +18,40 @@ class boardSpot(object):
         if boardSpot.value == -1:
             return True
         return False
+    
+    def getValue(self):
+        return boardSpot.value
 
 
 class boardClass(object):
-    def __init__(self, m_boardSize, m_numMines):
-        self.board = [[boardSpot() for i in range(m_boardSize)] for j in range(m_boardSize)]
-        self.boardSize = m_boardSize
+    def __init__(self, m_boardWidth, m_boardHeight, m_numMines, mineCoordinates=None):
+        self.board = [[boardSpot() for i in range(m_boardWidth)] for j in range(m_boardHeight)]
+        self.boardWidth = m_boardWidth
+        self.boardHeight = m_boardHeight
         self.numMines = m_numMines
-        self.selectableSpots = m_boardSize * m_boardSize - m_numMines
-        i = 0
-        while i < m_numMines:
-            x = random.randint(0, self.boardSize-1)
-            y = random.randint(0, self.boardSize-1)
-            if not self.board[x][y].mine:
+        self.numFlags = 0
+
+        if mineCoordinates:
+            for (x, y) in mineCoordinates:
                 self.addMine(x, y)
-                i += 1
-            else:
-                i -= 1
 
     def __str__(self):
         returnString = " "
         divider = "\n---"
 
-        for i in range(0, self.boardSize):
-            returnString += " | " + str(i)
+        for i in range(0, self.boardWidth):
             divider += "----"
         divider += "\n"
 
         returnString += divider
-        for y in range(0, self.boardSize):
-            returnString += str(y)
-            for x in range(0, self.boardSize):
-                if self.board[x][y].mine and self.board[x][y].selected:
-                    returnString += " |" + str(self.board[x][y].value)
-                elif self.board[x][y].selected:
-                    returnString += " | " + str(self.board[x][y].value)
+        for y in range(0, self.boardHeight):
+            for x in range(0, self.boardWidth):
+                if self.board[y][x].flagged:
+                    returnString += " | *"
+                elif self.board[y][x].isMine() and self.board[y][x].selected:
+                    returnString += " |" + str(self.board[y][x].value)
+                elif self.board[y][x].selected:
+                    returnString += " | " + str(self.board[y][x].value)
                 else:
                     returnString += " |  "
             returnString += " |"
@@ -58,68 +59,178 @@ class boardClass(object):
         return returnString
 
     def addMine(self, x, y):
-        self.board[x][y].value = -1
-        self.board[x][y].mine = True
+        self.board[y][x].value = -1
         for i in range(x-1, x+2):
-            if i >= 0 and i < self.boardSize:
-                if y-1 >= 0 and not self.board[i][y-1].mine:
-                    self.board[i][y-1].value += 1
-                if y+1 < self.boardSize and not self.board[i][y+1].mine:
-                    self.board[i][y+1].value += 1
-        if x-1 >= 0 and not self.board[x-1][y].mine:
-            self.board[x-1][y].value += 1
-        if x+1 < self.boardSize and not self.board[x+1][y].mine:
-            self.board[x+1][y].value += 1
+            if i >= 0 and i < self.boardWidth:
+                for j in range(y-1, y+2):
+                    if j >= 0 and j < self.boardHeight and not self.board[j][i].isMine():
+                        self.board[j][i].value += 1
 
     def makeMove(self, x, y):
-        self.board[x][y].selected = True
-        self.selectableSpots -= 1
-        if self.board[x][y].value == -1:
+        if self.board[y][x].flagged:
+            print("This spot is flagged. Unflag it first to make a move.")
+            return True
+        self.board[y][x].selected = True
+        if self.board[y][x].value == -1:
             return False
-        if self.board[x][y].value == 0:
+        if self.board[y][x].value == 0:
             for i in range(x-1, x+2):
-                if i >= 0 and i < self.boardSize:
-                    if y-1 >= 0 and not self.board[i][y-1].selected:
-                        self.makeMove(i, y-1)
-                    if y+1 < self.boardSize and not self.board[i][y+1].selected:
-                        self.makeMove(i, y+1)
-            if x-1 >= 0 and not self.board[x-1][y].selected:
-                self.makeMove(x-1, y)
-            if x+1 < self.boardSize and not self.board[x+1][y].selected:
-                self.makeMove(x+1, y)
-            return True
-        else:
-            return True
+                if i >= 0 and i < self.boardWidth:
+                    for j in range(y-1, y+2):
+                        if j >= 0 and j < self.boardHeight and not self.board[j][i].selected:
+                            self.makeMove(i, j)
+        return True
+    
+    def getSpot(self, x, y):
+        return self.board[y][x]
 
-    def hitMine(self, x, y):
-        return self.board[x][y].value == -1
+    def toggleFlag(self, x, y):
+        global numAllocations
+        if not self.getSpot(x, y).selected:
+            self.getSpot(x, y).flagged = not self.getSpot(x, y).flagged
+            if self.getSpot(x, y).flagged:
+                self.numFlags += 1
+            else:
+                self.numFlags -= 1
+        numAllocations += 1
 
-    def isWinner(self):
-        return self.selectableSpots == 0
+    def deepCopy(self):
+        return copy.deepcopy(self)
+
+    def getFlagPlacements(self):
+        return [(x, y) for y in range(self.boardHeight) for x in range(self.boardWidth) if self.board[y][x].flagged]
+    
+    def flagsAroundLocation(self, x, y):
+        mine_count = 0
+        for i in range(x-1, x+2):
+            if i >= 0 and i < self.boardWidth:
+                for j in range(y-1, y+2):
+                    if j >= 0 and j < self.boardHeight and self.getSpot(i, j).flagged:
+                        if not (i == x and j == y):
+                            mine_count += 1
+        return mine_count
+    
+    def isViablePath(self):
+        for y in range(self.boardHeight):
+            for x in range(self.boardWidth):
+                if self.getSpot(x, y).selected:
+                    mine_count = self.flagsAroundLocation(x, y)
+                    if mine_count > self.getSpot(x, y).value:
+                        return False
+        return True
+
+    def isConsistent(self):
+        for y in range(self.boardHeight):
+            for x in range(self.boardWidth):
+                if self.getSpot(x, y).selected:
+                    mine_count = self.flagsAroundLocation(x, y)
+                    if mine_count != self.getSpot(x, y).value:
+                        return False
+        return True
 
 
-#play game
-def playGame():
-    boardSize = int(input("Choose the Width of the board: "))
-    numMines = int(input("Choose the number of mines: "))
-    gameOver = False
-    winner = False
-    Board = boardClass(boardSize, numMines)
-    while not gameOver:
-        print(Board)
-        print("Make your move:")
-        x = int(input("x: "))
-        y = int(input("y: "))
-        Board.makeMove(x, y)
-        gameOver = Board.hitMine(x, y)
-        if Board.isWinner() and gameOver == False:
-            gameOver = True
-            winner = True
+# Method to create a small board with specific mine coordinates
+def smallBoard():
+    boardWidth = 4
+    boardHeight = 3
+    numMines = 3
+    mineCoordinates = [(0, 0), (1, 1), (3, 2)]
+    board = boardClass(boardWidth, boardHeight, numMines, mineCoordinates)
+    board.makeMove(0, 0)
+    board.makeMove(3, 0)
+    board.toggleFlag(1, 1)
+    return board
 
-    print(Board)
-    if winner:
-        print("Congratulations, You Win!")
+def smallBoardIncorrect():
+    board = smallBoard()
+    board.getSpot(0, 0).value = 5
+    return board
+
+def mediumBoard():
+    boardWidth = 10
+    boardHeight = 10
+    numMines = 6
+    mineCoordinates = [(1, 3), (5, 1), (5, 2), (1, 5), (2, 5), (4, 5), (5, 5)]
+    board = boardClass(boardWidth, boardHeight, numMines, mineCoordinates)
+    board.makeMove(0, 0)
+    return board
+
+def mediumBoardPartiallySolved():
+    board = mediumBoard()
+    board.toggleFlag(1, 3)
+    board.toggleFlag(5, 1)
+    board.toggleFlag(5, 2)
+    return board
+
+def backtrack(board, x=0, y=0, verbosity=0):
+        if verbosity > 1:
+            print("Board state:")
+            print(board)
+
+        # base case for when the board has been solved
+        if board.numFlags == board.numMines:
+
+            if board.isConsistent():
+                print("Solution found:")
+                print(board)
+                return board.getFlagPlacements()
+            else:
+                if verbosity > 0:
+                    print("Max mines reached, but board is inconsistent. Backtracking...")
+                return None
+        
+        # try every possible move
+        for y in range(board.boardHeight):
+            for x in range(board.boardWidth):
+                if(not (board.getSpot(x, y).selected or board.getSpot(x, y).flagged)):
+                    boardCopy = board.deepCopy()
+                    boardCopy.toggleFlag(x, y)
+                    if boardCopy.isViablePath():
+                        if verbosity > 0:
+                            print("Flagged spot at " + str(x) + ", " + str(y))
+                        result = backtrack(boardCopy, x, y, verbosity)
+                        if result:
+                            return result
+                    elif verbosity > 1:
+                        print("Attempted to flag spot at " + str(x) + ", " + str(y) + " but it is inconsistent")
+                        print(boardCopy)
+
+
+        # no moves found
+        if verbosity > 0:
+            print("No solution found on this path. Backtracking...")
+        return None
+
+def solveBacktracking(board, verbosity=0):
+    global numAllocations
+
+    numAllocations = 0
+    print("Starting board:")
+    print(board)
+    start_time = time.time()
+    answer = backtrack(board, verbosity=verbosity)
+    end_time = time.time()
+    if answer:
+        print("Flag locations:")
+        print(answer)
     else:
-        print("You hit a mine, Game Over!")
+        print("No solution found.")
+    print("Number of allocations: " + str(numAllocations))
+    print("Elapsed time: {:.4f} seconds".format(end_time - start_time))
 
-playGame()
+def testAlgorithms():
+    print("Testing backtracking on the following 4 by 3 board:")
+    solveBacktracking(smallBoard())
+
+    print("Testing backtracking on the following 4 by 3 invalid board:")
+    solveBacktracking(smallBoardIncorrect())
+
+    """
+    print("Testing backtracking on the following 10 by 10 board:")
+    solveBacktracking(mediumBoard(), 2)"""
+
+    print("Testing backtracking on the following 10 by 10 board:")
+    solveBacktracking(mediumBoardPartiallySolved())
+
+
+testAlgorithms()
